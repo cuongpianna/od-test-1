2c04f26b6f3369d51addffc46e33d289ca7b75b5
nicc777 <nicc777@gmail.com>
2019-05-20 11:26:06 +0200

    Updated for build

--- docker_conf/webapp/build_webapp.sh
+++ docker_conf/webapp/build_webapp.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+mkdir -p dist
+cd ../../
+python3 setup.py sdist
+cp -vf dist/* docker_conf/webapp/dist/
+cd $OLDPWD
+
 docker build --no-cache -t webapp-odtest1 .
--- od-test-1/webapp.py
+++ od-test-1/webapp.py
@@ -1,8 +1,16 @@
+from os import environ as env
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!' 
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=env.get('PORT', 5000))
 
 # EOF
