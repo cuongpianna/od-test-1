96d0dd1adc7d30bad9ed9b2c62a652c8b21a016c
nicc777 <nicc777@gmail.com>
2019-05-20 13:40:18 +0200

    WiP

--- od-test-1/webapp.py
+++ od-test-1/webapp.py
@@ -1,5 +1,6 @@
 from os import environ as env
 from functools import wraps
+import json
 from flask import Flask
 from flask import jsonify
 from flask import redirect
@@ -20,7 +21,8 @@ def get_debug_flag()->bool:
     return False
 
 
-app.debug = get_debug_flag()
+DEBUG_FLAG = get_debug_flag()
+app.debug = DEBUG_FLAG
 oauth = OAuth(app)
 auth0 = oauth.register(
     'auth0',
@@ -47,8 +49,9 @@ def requires_auth(f):
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!' 
+def home():
+    print('HOME rendering')
+    return render_template('home.html')
 
 
 @app.route('/callback')
@@ -57,6 +60,8 @@ def callback_handling():
     auth0.authorize_access_token()
     resp = auth0.get('userinfo')
     userinfo = resp.json()
+    if DEBUG_FLAG is True:
+        print('/callback - setting session with userinfo={}'.format(userinfo))
 
     # Store the user information in flask session.
     session['jwt_payload'] = userinfo
@@ -65,15 +70,53 @@ def callback_handling():
         'name': userinfo['name'],
         'picture': userinfo['picture']
     }
-    return redirect('/dashboard')
+    return redirect(url_for('dashboard'))
 
 
 @app.route('/login')
 def login():
+    redirect_uri = env.get('AUTH0_CALLBACK_URL', 'no-value')
+    audience = env.get('AUTH0_AUDIENCE', 'no-value')
+    if DEBUG_FLAG:
+        print('/login - Redirecting to "{}" with redirect_url={}'.format(audience, redirect_uri))
     return auth0.authorize_redirect(
-        redirect_uri=env.get('AUTH0_CALLBACK_URL', 'no-value'),
-        audience=env.get('AUTH0_AUDIENCE', 'no-value')
+        redirect_uri=redirect_uri,
+        audience=audience
     )
+
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    if DEBUG_FLAG is True:
+        print('/dashboard - profile: {}'.format(session['profile']))
+        print('/dashboard - jwt_payload: {}'.format(session['jwt_payload']))
+    return render_template(
+        'dashboard.html',
+        userinfo=session['profile'],
+        userinfo_pretty=json.dumps(
+            session['jwt_payload'],
+            indent=4
+        )
+    )
+
+
+@app.route('/logout')
+def logout():
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {
+        'returnTo': url_for('home', _external=True), 
+        'client_id': 'KGqKt6tqXfXQwFlKVyKKsXUUu0Wz1wf6',
+    }
+    final_uri = '{}/v2/logout?{}'.format(
+        auth0.api_base_url,
+        urlencode(params)
+    )
+    if DEBUG_FLAG:
+        print('/logout - redirecting to {}'.format(final_uri))
+    return redirect(final_uri)
 
 
 if __name__ == "__main__":
